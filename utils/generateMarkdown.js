8a02b9f78d7a916b787b6eb867a709c4071dbb61
Eric Ober <ericscottober@gmail.com>
2020-12-17 18:18:19 -0500

    Added function to generate markdown for readme.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,43 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  <h1 align="center"> ${data.title}</h1>
 
-`;
+  ![badge](https://img.shields.io/badge/license-${data.license}-blue)
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#install)
+  - [Usage](#usage)
+  - [Contribution](#contributors)
+  - [Test](#test)
+  - [License](#license)
+  - [Questions](#questions)
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributors
+  ${data.contributors}
+
+  ## Test
+  ${data.test}
+
+  ## License
+  ![badge](https://img.shields.io/badge/license-${data.license}-blue)
+
+  ## Questions
+  Github: https://github.com/${data.github}
+
+  Email: ${data.email}
+  
+  `;
 }
 
 module.exports = generateMarkdown;
