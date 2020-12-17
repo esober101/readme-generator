bb1c85f1d5ffb5b0b4363b5229d019f625580089
Eric Ober <ericscottober@gmail.com>
2020-12-17 18:13:59 -0500

    Added user prompt and questions.

--- index.js
+++ index.js
@@ -5,14 +5,57 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 const writeFileAsync = util.promisify(fs.writeFile);
 
-// array of questions for user
-const questions = [
-
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
+// User prompt 
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "Project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Project description?",
+        },
+        {
+            type: "input",
+            name: "install",
+            message: "Installation requirements?",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Project usage?",
+        },
+        {
+            type: "input",
+            name: "contributors",
+            message: "Contributors?",
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "How to test?",
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Select a license:",
+            choices: ["Apache", "GNU", "MIT"],
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your GitHub username.",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your email address.",
+        },
+    ]);
+};
 
 // function to initialize program
 function init() {
